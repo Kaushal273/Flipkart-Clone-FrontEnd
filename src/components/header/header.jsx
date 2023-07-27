ddac9811e1a5f8fdf57a27947c088bcfc9849a27
Kaushal273 <kaushalyadav536@gmail.com>
2023-07-27 18:57:50 +0530

    Second commit

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,4 +1,6 @@
 import { AppBar, Toolbar,Box ,Typography,styled } from "@mui/material";
+import Search from "./Search";
+import CustomButtons from "./CustomButtons";
 
 const StyledHeader = styled(AppBar)`
     background: #2874f0;
@@ -16,8 +18,14 @@ const SubHeading = styled(Typography)`
 `
 
 const PLusImage = styled('img')({
-    width:10
+    width:10,
+    height: 10,
+    marginLeft: 4
 })
+
+const CustomButtonWrapper = styled(Box)`
+    margin:0 5% 0 auto;
+`;
 
 
 
@@ -26,16 +34,20 @@ const Header = () => {
     const subURL= 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/plus_aef861.png';
     return (
         <StyledHeader>
-            <Toolbar>
+            <Toolbar style={{minHeight: 55}}>
                  <Component>
                     <img src={logoURL} alt="logo" style={{  width: 75 }} />
-                    <Box>
+                    <Box style={{display: "flex"}}>
                         <SubHeading>Explore&nbsp;
                             <Box component="span" style={{color: '#FFE500'}}>Plus</Box>
                             </SubHeading>
                             <PLusImage src={subURL} alt="sub-logo"/>
                     </Box>
                  </Component>
+                 <Search/>
+                 <CustomButtonWrapper>
+                    <CustomButtons />
+                 </CustomButtonWrapper>
             </Toolbar>
         </StyledHeader>
     )
