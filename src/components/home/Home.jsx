81a645dd758bdb8ddb38c58623a34f24959a1167
Kaushal273 <kaushalyadav536@gmail.com>
2023-08-05 21:29:23 +0530

    redux

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,6 +1,10 @@
 import NavBar from "./NavBar";
 import Banner from "./Banner";
 import {Box, styled } from "@mui/material";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getProducts } from "../../redux/actions/productActions";
+import Slide from "./Slide";
 
 const Component = styled(Box)`
     padding: 10px;
@@ -8,12 +12,22 @@ const Component = styled(Box)`
 `;
 
 const Home = () => {
+
+   const { products } =  useSelector(state => state.getProducts)                 //Here getproducts is the state in react-redux router DOM 
+   console.log(products)
+    const dispatch = useDispatch();
+
+    useEffect( () => {
+        dispatch(getProducts());                // this getProducts is a api function in productActions.js
+    },[dispatch])
+
     return(
         <>
 
             <NavBar/>
             <Component>
                 <Banner />
+                <Slide products = {products}/>
             </Component>
 
         </>
