d5fd0e7178123be8bf2ab497ec4c33c5729b7fee
Kaushal273 <kaushalyadav536@gmail.com>
2023-08-06 23:14:51 +0530

    Advertisement Added

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -5,6 +5,9 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getProducts } from "../../redux/actions/productActions";
 import Slide from "./Slide";
+import MidSlide from "./MidSlide";
+import MidSection from "./MidSection";
+
 
 const Component = styled(Box)`
     padding: 10px;
@@ -27,7 +30,16 @@ const Home = () => {
             <NavBar/>
             <Component>
                 <Banner />
-                <Slide products = {products}/>
+                <MidSlide products = {products} title="Deal of the Day" timer={true}/>
+                <MidSection />
+                <Slide products = {products} title="Discounts for You" timer={false}/>
+                <Slide products = {products} title="Suggested Items" timer={false}/>
+                <Slide products = {products} title="Top Selection" timer={false}/>
+                <Slide products = {products} title="Recommended Items" timer={false}/>
+                <Slide products = {products} title="Trending Offers" timer={false}/>
+                <Slide products = {products} title="Season's top picks" timer={false}/>
+                <Slide products = {products} title="Top Deals on Accessories" timer={false}/>
+                
             </Component>
 
         </>
---
+++ src/components/home/MidSection.jsx
@@ -0,0 +1,25 @@
+
+import { imageURL } from "../../constants/data";
+import {  Grid,styled } from "@mui/material";
+
+
+const Wrapper = styled(Grid)`
+    margin-top: 10px;
+    justify-content: space-between;
+`;
+
+const MidSection = () => {
+    return(
+        <Wrapper lg={12} sm={12} md={12} xs={12} container>
+            {
+                imageURL.map(image => (
+                    <Grid item lg={4} sm={12} md={4} xs={12}>
+                    <img src={image} alt="image" style={{width: '100%'}} />
+                    </Grid>
+                ))
+            }
+        </Wrapper>
+    )
+}
+
+export default MidSection;
