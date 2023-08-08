5c03402d25fc351f42e89abe5078756abef5ba3a
Kaushal273 <kaushalyadav536@gmail.com>
2023-08-08 23:13:33 +0530

    DetailView

--- src/App.js
+++ src/App.js
@@ -2,17 +2,23 @@
 //components
 import Header from './components/header/header';
 import Home from './components/home/Home';
-
+import DetailView from './components/details/DetailView';
 import { Box } from '@mui/material';
 import DataProvider from './context/DataProvider';
+import { BrowserRouter, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <DataProvider >
-      <Header />
-      <Box style = {{marginTop:54}}>
-        <Home />
-      </Box>
+      <BrowserRouter>
+        <Header />
+        <Box style={{ marginTop: 54 }}>
+          <Routes>
+            <Home />
+            <DetailView />
+          </Routes>
+        </Box>
+      </BrowserRouter>
     </DataProvider>
   );
 }
---
+++ src/components/details/DetailView.jsx
@@ -0,0 +1,11 @@
+
+
+const DetailView = () => {
+    return(
+        <div>
+            hello
+        </div>
+    )
+}
+
+export default DetailView;
