682c6997caeca61e6d48ccc6f42efe6108e7d0b8
Kaushal273 <kaushalyadav536@gmail.com>
2023-08-15 13:29:53 +0530

    cart-second-update

--- src/components/cart/CartItem.jsx
+++ src/components/cart/CartItem.jsx
@@ -1,17 +1,52 @@
-import { Box } from "@mui/material";
+import { Box, Button, Typography, styled } from "@mui/material";
+import { addEllipsis } from "../../utils/common-utils";
 
+const Component = styled(Box)`
+    border-top: 1px solid #f0f0f0;
+    display: flex;
+`;
 
+const LeftComponent = styled(Box)`
+    margin: 20px;
+`;
+
+const SmallText = styled(Typography)`
+    color: #878787;
+    font-size:14px;
+    margin-top: 10px;
+`;
+
+const Remove = styled(Button)`
+    margin-top: 20px;
+    font-size:16px;
+    color:#000;
+    font-weight: 600;
+`;
 
 const CartItem = ({ item }) => {
-    return(
-        <Box>
-            <Box>
-                <img src={item.url} alt="product" />
-            </Box>
-            <Box>
 
+    const fassured = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/fa_62673a.png';
+
+    return (
+        <Component>
+            <LeftComponent>
+                <img src={item.url} alt="product" />
+            </LeftComponent>
+            <Box margin={{margin : 20}}>
+                <Typography>{addEllipsis(item.title.longTitle)}</Typography>
+                <SmallText>Seller:RetailNet
+                    <Box component="span"><img src={fassured} alt="flipkart" style={{ width: 50, marginLeft: 10 }} /></Box>
+                </SmallText>
+
+                <Typography style={{ margin: '20px 0'}}>
+                    <Box component="span" style={{ fontWeight: 600, fontSize: 18 }}>₹{item.price.cost}</Box>&nbsp;&nbsp;&nbsp;
+                    <Box component="span" style={{ color: '#878787' }}><strike>₹{item.price.mrp}</strike></Box>&nbsp;&nbsp;&nbsp;
+                    <Box component="span" style={{ color: '#388E3C' }}>{item.price.discount}</Box>
+                </Typography>
+
+                <Remove>Remove</Remove>
             </Box>
-        </Box>
+        </Component>
     )
 }
 
